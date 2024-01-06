6515664407323e30d5997dabd7693584e0be3288
harshasj <36756863+harshasj@users.noreply.github.com>
2024-01-06 23:38:31 +0530

    Update WITH CREDENTIALS

--- variable.tf
+++ variable.tf
@@ -5,3 +5,11 @@ variable "instance_ami" {
 variable "type_instance" {
   type = string
 }
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
