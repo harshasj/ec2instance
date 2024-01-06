d1fcc098fc6adcdafeb63203400502900a113e2c
harshasj <36756863+harshasj@users.noreply.github.com>
2024-01-06 14:25:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,5 @@
-resource "aws_instance" "web" {
-  ami                    = var.instance_ami
-  instance_type          = var.type_instance
-  user_data              = <<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    sudo apt install git -y
-    sudo apt install nginx -y
-    EOF 
-    
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-resource "aws_security_group" "allow_all_traffic" {
-  name        = "allow_all_traffic"
-  description = "Allow all inbound traffic"
-
-  ingress {
-    description      = "all traffic "
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    description      = "all traffic "
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_all_traffic"
-  }
+module "ec2instance" {
+  source  = "app.terraform.io/infra_demo/ec2instance/aws"
+  version = "1.0.0"
+  # insert required variables here
 }
