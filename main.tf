7b048629190f8150b41ef5ce9678d35d73c5e5f7
harshasj <36756863+harshasj@users.noreply.github.com>
2024-01-06 14:28:30 +0530

    add mising variables to module

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ module "ec2instance" {
   source  = "app.terraform.io/infra_demo/ec2instance/aws"
   version = "1.0.0"
   # insert required variables here
+  instance_ami = var.instance_ami
+  type_instance = var.type_instance
 }
